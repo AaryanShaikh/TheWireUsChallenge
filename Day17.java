0e38cf67cf1e3c483d3171ccb6ebffbf5d86d659
Aaryan Shaikh <53545857+AaryanShaikh@users.noreply.github.com>
2020-10-05 00:41:45 +0530

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Day17{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String str = in.nextLine();
+		int i=0;
+		int j=0;
+		int n=0;
+		while(true){
+			j=i;
+			while(j<str.length()){
+				if(i==j){
+					System.out.println(str.charAt(j));
+					j++;
+				}
+				else{
+					n = j;
+					for (int k=i;k<=n;k++ ) {
+						System.out.print(str.charAt(k));
+					}
+					System.out.println();
+					j++;
+				}
+			}
+			i++;
+			if (i==str.length()) {
+				break;
+			}
+		}
+	}
+}
